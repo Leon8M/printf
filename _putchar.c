467d3ac24bde5023fce2c0d6e09bc6895be2f926
Phosteve <125594974+Phosteve@users.noreply.github.com>
2023-04-25 22:58:17 +0300

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -2,7 +2,7 @@
 
 /**
  *_putchar - writes character c to stdout
- *@c: The character to print
+ *@c: The character to print.
  *Return: On success 1.
  *On error, -1 is returned, and errno is set appropriately
  */
