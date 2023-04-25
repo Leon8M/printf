910e2e8ee899e3d7f366a596d09848a007f1d987
Leon Munene <leonmunene254@gmail.com>
2023-04-25 03:06:22 -0700

    2

--- main.h
+++ main.h
@@ -1,9 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUFFER_SIZE 1024
+
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdlib.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
+int _printf_five(const char *format, ...);
+int _printf_seven(const char *format, ...);
+int _printf_thirteenth(const char *format, ...);
+
 #endif /* MAIN_H */
